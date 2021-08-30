e2541420bb9e49f326ff33261e96418a1f1a6aa2
kachuedream <88540505+kachuedream@users.noreply.github.com>
2021-08-30 17:28:02 +0700

    Update chawong26I.c

--- chawong26I.c
+++ chawong26I.c
@@ -7,29 +7,29 @@ int main()
     for(int i=0;i<n-1;i++)
     {
         for(int j=0;j<((i+1)*2)/2;j++)
-        printf("* ");
+        printf("*");
 
         for(int j=0;j<(2*n-3)-(i*2);j++)
-        printf("  ");
+        printf(" ");
 
         for(int j=0;j<((i+1)*2)/2;j++)
-        printf("* ");
+        printf("*");
         printf("\n");
     }
     for(int i=0;i<n*2-1;i++)
-    printf("* ");
+    printf("*");
     printf("\n");
 
     for(int i=n-2;i>=0;i--)
     {
         for(int j=0;j<((i+1)*2)/2;j++)
-        printf("* ");
+        printf("*");
 
         for(int j=0;j<(2*n-3)-(i*2);j++)
-        printf("  ");
+        printf(" ");
 
         for(int j=0;j<((i+1)*2)/2;j++)
-        printf("* ");
+        printf("*");
         printf("\n");
     
     }
