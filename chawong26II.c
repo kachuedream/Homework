3c1e61a1f9b2e66dd187e84d8c2b4500c6fa9b16
kachuedream <88540505+kachuedream@users.noreply.github.com>
2021-08-30 17:24:51 +0700

    Create chawong26II.c

---
+++ chawong26II.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int boxsize = n*2-1;
+    for(int i=1;i<=boxsize;i++)
+    {
+        for(int j=1;j<=boxsize;j++)
+        {
+            if((j<=i && j<=n*2-i)||(j>=i && j>=n*2-i))
+            {
+                printf("* ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
