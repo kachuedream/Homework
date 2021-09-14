c8ca517b955112821504b23d316766f26cc3c6d3
kachuedream <88540505+kachuedream@users.noreply.github.com>
2021-09-14 17:41:07 +0700

    Create chawong25II.c

---
+++ chawong25II.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<2*n-1;j++)
+        {
+            if((i==0 && j==n-1)|| i==n-1 || i>=n-1-j&&i>=j-n+1)
+            {
+                printf("*");
+            }
+            else
+            {
+            printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
